0673421c78bd8463ba8e52749416035a20251cff
Praneeth <johndoeknown@protonmail.com>
2021-02-13 17:37:54 +0530

    fixed background color issue

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,11 @@ import CustomHead from "../components/CustomHead";
 import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }) {
-    return <div className="bg-blue-100 h-full min-h-screen">
+    return <>
         <CustomHead />
         <Header />
         <Component {...pageProps} />
-    </div>
+    </>
 }
 
 export default MyApp
